92482d6a4e415f3f991e8801ede88f0850621f17
Mitchell Crumbley <76921222+Mitchell-Crumbley@users.noreply.github.com>
2021-03-20 09:14:43 -0500

    Revert "Mjs vendor setup"

--- src/javascripts/events/navigationEvents.js
+++ src/javascripts/events/navigationEvents.js
@@ -1,8 +1,6 @@
 import 'firebase/auth';
 import createRides from '../components/cards/rides';
 import signOut from '../helpers/auth/signOut';
-import { renderVendors, emptyVendors } from '../components/cards/vendor';
-import { getVendors } from '../helpers/data/vendorData/vendorData';
 import { getRides } from '../helpers/data/rideData/ridesData';
 
 const navigationEvents = () => {
@@ -35,17 +33,6 @@ const navigationEvents = () => {
   // LOGOUT BUTTON
   document.querySelector('#logout-button')
     .addEventListener('click', signOut);
-
-  // GET VENDORS
-  document.querySelector('#vendors-link').addEventListener('click', () => {
-    getVendors().then((vendorArray) => {
-      if (vendorArray.length) {
-        renderVendors(vendorArray);
-      } else {
-        emptyVendors();
-      }
-    });
-  });
 };
 
 export default navigationEvents;
--- src/javascripts/views/startApp.js
+++ src/javascripts/views/startApp.js
@@ -4,8 +4,6 @@ import domBuilder from '../components/domBuilder';
 import navBar from '../components/navBar';
 import domEvents from '../events/domEvents';
 import navigationEvents from '../events/navigationEvents';
-import { renderVendors, emptyVendors } from '../components/cards/vendor';
-import { getVendors } from '../helpers/data/vendorData/vendorData';
 import { getRides } from '../helpers/data/rideData/ridesData';
 
 const startApp = (userObject) => {
@@ -13,14 +11,6 @@ const startApp = (userObject) => {
   navBar();
   domEvents();
   navigationEvents();
-
-  getVendors(userObject).then((vendorArray) => {
-    if (vendorArray.length) {
-      renderVendors(vendorArray);
-    } else {
-      emptyVendors(userObject);
-    }
-  });
   logoutButton();
   getRides(userObject).then((rides) => createRides(rides));
 };
