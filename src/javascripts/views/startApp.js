fd5de62fd5975581d3dc236cd3c4376bae1eef82
Mitchell Crumbley <76921222+Mitchell-Crumbley@users.noreply.github.com>
2021-03-20 10:22:19 -0500

    Revert "Revert "Mjs vendor setup""

--- src/javascripts/views/startApp.js
+++ src/javascripts/views/startApp.js
@@ -4,6 +4,8 @@ import domBuilder from '../components/domBuilder';
 import navBar from '../components/navBar';
 import domEvents from '../events/domEvents';
 import navigationEvents from '../events/navigationEvents';
+import { renderVendors, emptyVendors } from '../components/cards/vendor';
+import { getVendors } from '../helpers/data/vendorData/vendorData';
 import { getRides } from '../helpers/data/rideData/ridesData';
 
 const startApp = (userObject) => {
@@ -11,6 +13,14 @@ const startApp = (userObject) => {
   navBar();
   domEvents();
   navigationEvents();
+
+  getVendors(userObject).then((vendorArray) => {
+    if (vendorArray.length) {
+      renderVendors(vendorArray);
+    } else {
+      emptyVendors(userObject);
+    }
+  });
   logoutButton();
   getRides(userObject).then((rides) => createRides(rides));
 };
