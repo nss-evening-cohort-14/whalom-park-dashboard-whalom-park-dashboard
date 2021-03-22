0df1a4b168fef40eebfe445f3ac5951fb2a6db45
fimoefive <brothermj@hotmail.com>
2021-03-22 14:57:36 -0500

    deleted the $ when uncheck the vendor active sign for the Vendor product.

--- src/javascripts/components/cards/vendor.js
+++ src/javascripts/components/cards/vendor.js
@@ -12,7 +12,7 @@ const renderVendors = (venArray) => {
         <img class="card-img-top" src=${item.vendorImageURL} alt=${item.vendorName} style="height: 400px;">
         <div class="card-body--${item.firebaseKey}" style="height: 180px;">
         <h5 class="card-title">${item.vendorName}</h5>
-        <p class="card-text bold">${item.vendorIsActive ? `<span class="badge badge-info product-badge"><i class="fa fa-bell" aria-hidden="true"></i> Active</span> ${item.vendorProduct}` : `$${item.vendorProduct}`}</p>
+        <p class="card-text bold">${item.vendorIsActive ? `<span class="badge badge-info product-badge"><i class="fa fa-bell" aria-hidden="true"></i> Active</span> ${item.vendorProduct}` : `${item.vendorProduct}`}</p>
         <hr>
         <button class="btn btn-info" data-toggle="modal" data-target="#formModal" id="edit-vendor-btn--${item.vendorID_firebaseKey}">Edit Vendor</button>
         <button class="btn btn-danger" id="delete-vendor--${item.firebaseKey}">Delete Vendor</button>
