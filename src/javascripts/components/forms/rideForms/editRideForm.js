9d3ceb46c6b49ef279a88129cb069bd4cebd4013
Mitchell-Crumbley <mitchellcrumbley@gmail.com>
2021-03-22 21:04:02 -0500

    random changes

--- src/javascripts/components/forms/rideForms/editRideForm.js
+++ src/javascripts/components/forms/rideForms/editRideForm.js
@@ -12,8 +12,6 @@ const editRideForm = (ridesObject) => {
   <label for='image'>Image URL</label>
   <input type='url' class='form-control' id='image' placeholder='Image URL' required value='${ridesObject.rideImageURL}'>
 </div>
-<div class='form-group' id='select-staff'></div>
-<div class='form-group' id='select-staff-img'></div>
     <button type='button' id='update-ride--${ridesObject.rideID_firebaseKey}' class='btn btn-danger'>Update Ride</button>`;
   // selectStaff(ridesObject);
 };
