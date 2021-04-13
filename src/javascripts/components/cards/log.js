ae7c631546aa4cd37e3ed05213dc5f5bdf3df492
HunterJuneau <huntlee17@gmail.com>
2021-04-12 21:19:35 -0500

    Added amCharts and formatted performance page.

--- src/javascripts/components/cards/log.js
+++ src/javascripts/components/cards/log.js
@@ -1,7 +1,18 @@
 const showPerformance = () => {
   document.querySelector('#header').innerHTML = '<h1>Performance</h1>';
-  document.querySelector('#add-button').innerHTML = '';
-  document.querySelector('#display-area').innerHTML = '<h5>Visitor Tracking</h5><h5>Performance Charting</h5>';
+  document.querySelector('#add-button').innerHTML = `<ul class="nav nav-pills nav-justified w-25 mx-auto">
+                                                      <li class="nav-item">
+                                                        <a class="nav-link active" aria-current="page" href="#">Income Charts</a>
+                                                      </li>
+                                                      <li class="nav-item">
+                                                        <a class="nav-link" href="#">Transaction Logs</a>
+                                                      </li>
+                                                    </ul>`;
+  document.querySelector('#display-area').innerHTML = `<div>
+                                                        <div id="totalRevenue"></div>
+                                                        <div id="rideRevenue"></div>
+                                                        <div id="vendorRevenue"></div>
+                                                      </div>`;
 };
 
 export default showPerformance;
